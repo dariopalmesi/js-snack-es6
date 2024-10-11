9089f7d381f83619048cce1577dbdddb553f14ce
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-11 16:02:42 +0200

    initial commit

---
+++ snack 1/main.js
@@ -0,0 +1,38 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+
+
+const biciclette = [
+    {
+        "nome": "Mountain Bike Pro",
+        "peso": "13 kg"
+    },
+    {
+        "nome": "Bici da Corsa Ultra",
+        "peso": "7.5 kg"
+    },
+    {
+        "nome": "Bici Elettrica City",
+        "peso": "24 kg"
+    }
+]
+
+
+const biciRowEl = document.querySelector('.row')
+console.log(biciRowEl);
+
+for (let i = 0; i < biciclette.length; i++) {
+    const bici = biciclette[i];
+    console.log(bici.nome, bici.peso);
+
+    const markup = `
+    <div class="col">
+                    <div class="card">
+                        <p>${bici.peso}</p>
+                    </div>
+                </div>
+    `
+}  
+biciRowEl.innerHTML(markup)
